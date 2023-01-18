634b737e46694f3af89f1b4d83a10b188164a394
tugyanozsoy <tugyanozsoy@yahoo.com>
2023-01-18 13:58:19 +0300

    Version düzeltme

--- src/test/java/Pages/DialogContent.java
+++ src/test/java/Pages/DialogContent.java
@@ -7,24 +7,46 @@ import org.openqa.selenium.support.PageFactory;
 
 public class DialogContent extends Parent {
 
-    public DialogContent(){
-        PageFactory.initElements(GWD.getDriver(),this);
+    WebElement myElement;
+
+    public DialogContent() {
+        PageFactory.initElements(GWD.getDriver(), this);
     }
 
     @FindBy(id = "mat-input-0")
-    public WebElement username;
+    private WebElement username;
 
     @FindBy(id = "mat-input-1")
-    public WebElement password;
+    private WebElement password;
 
     @FindBy(css = "button[aria-label=\"LOGIN\"]")
-    public WebElement loginButton;
+    private WebElement loginButton;
 
-    @FindBy(xpath = "mat-input-2")
-    public WebElement dasboard;
+    @FindBy(xpath="(//span[contains(text(),'Dashboard')])[2]")
+    private WebElement dashboard;
 
+    public void findAndSendKeys(String strElement, String value) {
+        switch (strElement) {
+            case "username": myElement = username;break;
+            case "password": myElement = password;break;
+        }
 
+        sendKeysFunction(myElement, value);
+    }
 
+    public void findAndClick(String strElement){
+        switch (strElement){
+            case "loginButton" : myElement = loginButton;break;
+        }
+        clickFunction(myElement);
+    }
 
+    public void findAndVerify(String strElement,String text){
+        switch (strElement)
+        {
+            case "dasboard" : myElement = dashboard;break;
+        }
+        verifyContainsText(myElement,text);
+    }
 
 }
--- src/test/java/StepDefinitions/_01_LoginSteps.java
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -5,10 +5,6 @@ import Utilities.GWD;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public class _01_LoginSteps {
 
@@ -22,15 +18,13 @@ public class _01_LoginSteps {
 
     @When("User enter username and password and click login button")
     public void userEnterUsernameAndPasswordAndClickLoginButton() {
-        WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(30));
-        wait.until(ExpectedConditions.visibilityOf(dc.username));
-        dc.username.sendKeys("turkeyts");
-        dc.password.sendKeys("TechnoStudy123");
-        dc.loginButton.click();
+        dc.findAndSendKeys("username","turkeyts");
+        dc.findAndSendKeys("password","TechnoStudy123");
+        dc.findAndClick("loginButton");
     }
 
     @Then("User should login successfully")
     public void userShouldLoginSuccessfully() {
-
+        dc.findAndVerify("dasboard","Dashboard");
     }
 }
